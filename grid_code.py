5d8e9574f031c439ce8980bb9f8cd19a36a53802
Benjamin Howell <ben.howell@utexas.edu>
2022-04-20 17:38:47 -0500

    Create grid_code.py

---
+++ grid_code.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+# code that breaks the hockey rink up into 80 different boxes
+
+xbreaks = np.linspace(start=-100, stop=100, num=11)
+ybreaks = np.linspace(-42.5, 42.5, num=8)
+
